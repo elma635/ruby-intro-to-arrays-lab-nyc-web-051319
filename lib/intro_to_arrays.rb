e1157940f7082ed589703ad234c076291b0a0e97
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 23:30:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,7 +15,6 @@ def array_with_two_elements
 animals = ["lizard" , "bird"]
 end
 
-down_the_block = ["candy", "store"]
 
 def vacation_house(array)
   @vacation_house = ["bbq", "beach","umbrella", "crabs"]
