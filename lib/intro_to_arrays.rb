0bcb85fb7d1ef467317291b423bd82d4f457dc72
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 22:14:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,8 +10,8 @@ return
 
 @down_the_block = ["candy", "store"]
 
-@vacation_house = ["bbq", "beach","umbrella"]
+@vacation_house = ["bbq", "beach","umbrella", "crabs"]
 
 my_first_element = my_first_element(@vacation_house)
 
-my_third_element = my_third_element()
+my_third_element = my_third_element(@vacation_house)
