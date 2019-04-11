71bd6ef1b7c2cf9ef6e9da92634c4fa94dfc9239
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 22:57:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,10 +17,18 @@ down_the_block = ["candy", "store"]
 def indexing
   vacation_house = ["bbq", "beach","umbrella", "crabs"]
 end
-my_first_element = my_first_element(vacation_house)
 
-my_third_element = my_third_element(vacation_house)
+def my_first_element
+  my_first_element(vacation_house)
+end
 
-my_last_element = my_last_element(vacation_house)
+def my_third_element
+ my_third_element(vacation_house)
+ end
+
+def my_last_element
+   my_last_element(vacation_house)
+end
+
 
 @groceries = ["bread", "milk", "eggs", "meat"]
