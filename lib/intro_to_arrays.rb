d513e458902774fe38728e9283f1dbf1ba801716
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 22:10:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,7 +1,11 @@
 
-  @my_new_array = instantiate_new_array
+
+@my_new_array = instantiate_new_array
 my_new_array = ["cat", "dog", "mice","spider"]
 
 my_new_array = [ ]
 
 my_new_array = ["lizard" , "bird"]
+return
+
+my_new_array = ["candy", "store"]
