1d1a801a809464e4f5c5b74b60a73cb70aa5289d
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 22:55:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,21 +1,21 @@
 
 
-@my_new_array = instantiate_new_array
+def creates_a_new_array_and_returns_it
 my_new_array = ["cat", "dog", "mice","spider"]
 
 my_new_array = [ ]
 
-@animals = ["lizard" , "bird"]
+animals = ["lizard" , "bird"]
 return
 
 @down_the_block = ["candy", "store"]
 
-@vacation_house = ["bbq", "beach","umbrella", "crabs"]
+vacation_house = ["bbq", "beach","umbrella", "crabs"]
 
-my_first_element = my_first_element(@vacation_house)
+my_first_element = my_first_element(vacation_house)
 
-my_third_element = my_third_element(@vacation_house)
+my_third_element = my_third_element(vacation_house)
 
-my_last_element = my_last_element(@vacation_house)
+my_last_element = my_last_element(vacation_house)
 
 @groceries = ["bread", "milk", "eggs", "meat"]
